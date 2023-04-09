3b765b8b9c85d2c6d6c1cdbbbd2c816bdbfd6144
Vivek <vivekacoustic@gmail.com>
2023-04-10 01:08:23 +0530

    spring boot project

---
+++ src/main/java/cons/demo/FirstProjApplication.java
@@ -0,0 +1,13 @@
+package cons.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cons/demo/FirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package cons.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
